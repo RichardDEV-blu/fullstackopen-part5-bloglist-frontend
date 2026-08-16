f9bd11c74a975bba317526c41fe4d2e0f3824fde
RichardDEV-blu <arteagaricharda@gmail.com>
2026-08-16 10:55:49 -0500

    part 5: 5.6 - 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  return (
+    <div>
+      <div>
+        {blog.title} {blog.author}
+      </div>
+
+      <div style={hideWhenVisible}>
+        <button onClick={() => setVisible(true)}>
+          view
+        </button>
+      </div>
+
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>{blog.likes} likes</div>
+        <div>{blog.user.name}</div>
+
+        <button>like</button>
+
+        <button onClick={() => setVisible(false)}>
+          hide
+        </button>
+      </div>
+    </div>)
+}
 
 export default Blog
