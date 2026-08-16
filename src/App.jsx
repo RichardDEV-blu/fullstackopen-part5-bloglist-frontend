07f0d604100c0290257040adde48420eb5492f92
RichardDEV-blu <arteagaricharda@gmail.com>
2026-08-16 12:32:36 -0500

    part 5: 5.9 - 5.10

--- src/App.jsx
+++ src/App.jsx
@@ -112,9 +112,16 @@ const App = () => {
       <Togglable buttonLabel="create new blog">
         <BlogForm createBlog={createBlog} />
       </Togglable>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} token={user.token} likeBlog={likeBlog} />
-      )}
+      {[...blogs].sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog
+            key={blog.id}
+            blog={blog}
+            likeBlog={likeBlog}
+          />
+        )}
+
+
     </div>
   )
 }
